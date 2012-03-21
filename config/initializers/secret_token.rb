fe1aac8fbb3a9dac1477c85fdd4d27dcfeec9719
Yoshiori SHOJI <yoshiori@gmail.com>
2012-03-21 22:04:14 +0900

    初期登録

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Shelf::Application.config.secret_token = '6432b7267d40335ee1c2445162a6c0ab076b8a471c23ae6f426cd57001abeb5565d6f69de01132ee8b9df0bca1f695144ec825cb4fe0334ced748286a02e7f19'
