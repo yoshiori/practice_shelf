9ace897b18da08bff62f6585e2367768773f8b95
Yoshiori SHOJI <yoshiori@gmail.com>
2012-03-22 11:58:58 +0900

    8 章の演習問題終了

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -6,4 +6,13 @@ class BooksController < ApplicationController
   def show
     @book = Book.find(params[:id])
   end
+
+  def new
+    @book = Book.new
+  end
+
+  def create
+    @book = Book.create(params[:book])
+    redirect_to @book
+  end
 end
