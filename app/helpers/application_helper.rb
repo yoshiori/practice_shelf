442392f62303f12cdde9ebe6b648ae973176f556
Yoshiori SHOJI <yoshiori@gmail.com>
2012-03-23 21:03:03 +0900

    13 章の演習問題終了

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,6 @@
+# -*- coding: utf-8 -*-
 module ApplicationHelper
+  def delete_link(object)
+    link_to "削除", object, :method => :delete, :confirm => "本当に削除しますか？", :class => "btn btn-danger"
+  end
 end
--- app/helpers/books_helper.rb
+++ app/helpers/books_helper.rb
@@ -1,2 +1,14 @@
+# -*- coding: utf-8 -*-
 module BooksHelper
+  def links_for(book)
+    links = []
+    links << link_to( "修正", [ :edit, book ], :class => "btn btn-success")
+    if book.checked_out? 
+      links << link_to( "在庫", [ :check_in, book ], :method => :put, :class => "btn btn-inverse")
+    else
+      links << link_to( "貸出中", [ :check_out, book ], :method => :put, :class => "btn btn-inverse")
+    end
+    links << delete_link( book )
+    raw(links.join(" "))
+  end
 end
